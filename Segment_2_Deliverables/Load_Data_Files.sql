4c7d10ba1188954a08a46025009b662edc14493b
Vanessa Neang <neangvanessa@gmail.com>
2023-01-07 22:38:21 -0800

    connected database to elephantSQL and merged datafiles

--- Segment_2_Deliverables/Load_Data_Files.sql
+++ Segment_2_Deliverables/Load_Data_Files.sql
@@ -1,8 +1,17 @@
-CREATE TABLE clean_merge_df(
-    Year date,
-    Populateion_size FLOAT,
-    Unemployment_Rate FLOAT,
-    Median_HOusehold_Income FLOAT,
-    County VARCHAR,
-    First_Time_Buyer_Rate FLOAT
-)
+--Merge two tables in SQL
+CREATE TABLE complete_housing_data AS
+SELECT clean_merge_df."Year", clean_merge_df."County",clean_merge_df."First_Time_Buyer_Rate",clean_merge_df."Median_Household_Income",
+clean_merge_df."Population_Size", clean_merge_df."Unemployment_Rate",median_house_df."Median_House_Price",
+median_house_df."CPI-U"
+FROM clean_merge_df
+LEFT OUTER JOIN
+    median_house_df 
+ON
+    clean_merge_df."County" = median_house_df."County" AND clean_merge_df."Year" = median_house_df."Year"
+	
+SELECT * FROM complete_housing_data
+-- Order by county and year
+SELECT * FROM complete_housing_data
+ORDER BY ("County"),("Year") ASC
+
+-- DROP TABLE complete_housing_data
