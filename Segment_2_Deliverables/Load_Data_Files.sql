f1787194c9b5d5366256b38feed820f85f7cb376
Vanessa Neang <neangvanessa@gmail.com>
2023-01-11 16:41:50 -0800

    change file locations

--- Segment_2_Deliverables/Load_Data_Files.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE clean_merge_df(
-    Year date,
-    Populateion_size FLOAT,
-    Unemployment_Rate FLOAT,
-    Median_HOusehold_Income FLOAT,
-    County VARCHAR,
-    First_Time_Buyer_Rate FLOAT
-)
