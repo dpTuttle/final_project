d00dd716693ca5b0edfa23fbecbfc25f5cd5b98d
Vanessa Neang <neangvanessa@gmail.com>
2023-01-12 14:39:53 -0800

    updated code with new complete data file

--- Segment_2_Deliverables/Database_Files/Load_Data_Files.sql
+++ Segment_2_Deliverables/Database_Files/Load_Data_Files.sql
@@ -1,8 +1,18 @@
-CREATE TABLE clean_merge_df(
-    Year date,
-    Populateion_size FLOAT,
-    Unemployment_Rate FLOAT,
-    Median_HOusehold_Income FLOAT,
-    County VARCHAR,
-    First_Time_Buyer_Rate FLOAT
-)
+SELECT * FROM clean_merge_df
+
+SELECT * FROM median_house_df
+
+CREATE TABLE complete_ca_data AS
+    SELECT clean_merge_df."Year", 
+	clean_merge_df."County",
+	clean_merge_df."Population_Size",
+	clean_merge_df."Unemployment_Rate",
+	clean_merge_df."Median_Household_Income",
+	median_house_df."CPI", median_house_df."Median_House_Price"
+	FROM clean_merge_df
+	INNER JOIN median_house_df
+	ON median_house_df."Year" = clean_merge_df."Year" 
+	AND median_house_df."County" = clean_merge_df."County"
+	
+SELECT * FROM complete_ca_data
+	
